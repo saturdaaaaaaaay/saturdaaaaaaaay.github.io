cd28b1bc2e20d590ddc7fc56d2f0b256801bf5d1
Elie Carlos <ecc226@nau.edu>
2025-02-20 11:11:18 -0700

    Add gh-pages

--- next-react-project/next.config.mjs
+++ next-react-project/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        unoptimized: true
+    },
+    assetprefix: isProd ? '/react_website_test/' : '',
+    basePath: isProd ? '/react_website_test' : '',
+    output: 'export'
+};
 
 export default nextConfig;
